70a3d69a6f003c8a69504c27467bd36e5ef7e5f4
profetecno2 <123400029+profetecno2@users.noreply.github.com>
2023-06-05 13:58:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    JugadorArriba.change(LedSpriteProperty.Y, -1)
+    JugadorAbajo.change(LedSpriteProperty.Y, -1)
+})
+let JugadorAbajo: game.LedSprite = null
+let JugadorArriba: game.LedSprite = null
+JugadorArriba = game.createSprite(1, 3)
+JugadorAbajo = game.createSprite(1, 4)
+let JugadorObstaculo = game.createSprite(4, 4)
 basic.forever(function () {
-    music.playMelody("- - - - - - - - ", 120)
+	
 })
